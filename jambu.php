3603412d386543e25ba31a40f31fb999e8bdd382
Diki Maulana <tyrantsid11@gmail.com>
2019-08-01 17:05:35 +0700

    new

--- jambu.php
+++ jambu.php
@@ -3,10 +3,10 @@
 $harga=15000;
 $jml=6*5;
 $dus=2000*6;
-$jmb=$jmlh*$harga;
+$jmb=$jml*$harga;
 $total=$dus+$jmb;
 
 echo"harga kardus = ".$dus;
-echo"harga jambu = ".$jmb;
-echo"total penjualan = ".$total;
+echo"<br>harga jambu = ".$jmb;
+echo"<br>total penjualan = ".$total;
 
--- umur.php
+++ umur.php
@@ -1,13 +1,17 @@
 <?php
+$nolkoma=0.5;
 $toni=$tama*$nolkoma;
 $tini=$tama-$beda;
-$tama=$toti+$beda;
-$nolkoma=0.5;
+
 $beda=14;
 $toti=34;
+
+$tama=$toti+$beda;
 $tama2=$tama/1.5;
-
-echo"umur tama 34+14=".$tama."<br>32/".$nolkoma."=".$tama2;
-
+echo"umur tama 34+14=".$tama."<br>34/".$nolkoma."=".$tama2;
+echo"<br>tama = ".$tama2;
+echo"<br>tono = ".$tama2/2;
+echo"<br>tini= ".$tini;
+ 
 
 
