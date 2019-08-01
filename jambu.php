df48eb1428ca1a70e5df3e02da9cffb18874a7db
Diki Maulana <tyrantsid11@gmail.com>
2019-08-01 16:51:18 +0700

    latihan php

---
+++ jambu.php
@@ -0,0 +1,12 @@
+<?php
+
+$harga=15000;
+$jml=6*5;
+$dus=2000*6;
+$jmb=$jmlh*$harga;
+$total=$dus+$jmb;
+
+echo"harga kardus = ".$dus;
+echo"harga jambu = ".$jmb;
+echo"total penjualan = ".$total;
+
---
+++ umur.php
@@ -0,0 +1,13 @@
+<?php
+$toni=$tama*$nolkoma;
+$tini=$tama-$beda;
+$tama=$toti+$beda;
+$nolkoma=0.5;
+$beda=14;
+$toti=34;
+$tama2=$tama/1.5;
+
+echo"umur tama 34+14=".$tama."<br>32/".$nolkoma."=".$tama2;
+
+
+
