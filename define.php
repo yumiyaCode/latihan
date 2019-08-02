de0768651fb8cfdcfa693e28479baeab4f86fd3e
Diki Maulana <tyrantsid11@gmail.com>
2019-08-02 16:43:39 +0700

    tugas

--- define.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("NAMA","ahmad");
-define("NILAI","90");
-
-
-
- echo"Nama = " .NAMA;
- echo"<br>Nilai = " .NILAI;
- ?>
--- segitiga.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$alas =10;
-$tinggi = 15;
-$luas =($alas*$tinggi)/2;
-
-echo"Alas   :$alas <br>";
-echo"Tinggi :$tinggi<br>";
-echo "Luas  :alas x tinggi / 2 = ". $luas;
-?>  
